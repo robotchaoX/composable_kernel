1a67dd09affa6434cbf0a332a2f5d1cd8bbb1abd
Qun Lin <qlin@amd.com>
2025-08-01 10:40:08 +0800

    fix clang format 2

--- example/ck_tile/05_reduce/reduce.cpp
+++ example/ck_tile/05_reduce/reduce.cpp
@@ -63,16 +63,16 @@ bool run(const ck_tile::ArgParser& arg_parser)
 
     using Kernel = ck_tile::Reduce<Porblem>;
 
-    float ave_time = launch_kernel(ck_tile::stream_config{nullptr, true, 0, warmup, repeat},
-                                   ck_tile::make_kernel<kBlockPerCu>(
-                                       Kernel{},
-                                       kGridSize,
-                                       kBlockSize,
-                                       0,
-                                       static_cast<XDataType*>(x_buf.GetDeviceBuffer()),
-                                       static_cast<YDataType*>(y_buf.GetDeviceBuffer()),
-                                       m,
-                                       n));
+    float ave_time = launch_kernel(
+        ck_tile::stream_config{nullptr, true, 0, warmup, repeat},
+        ck_tile::make_kernel<kBlockPerCu>(Kernel{},
+                                          kGridSize,
+                                          kBlockSize,
+                                          0,
+                                          static_cast<XDataType*>(x_buf.GetDeviceBuffer()),
+                                          static_cast<YDataType*>(y_buf.GetDeviceBuffer()),
+                                          m,
+                                          n));
 
     std::size_t num_btype = sizeof(XDataType) * m * n + sizeof(YDataType) * m;
 
